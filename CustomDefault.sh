dce76f56dd34c5bef49fca97f307268376968080
vki888 <31002355+vki888@users.noreply.github.com>
2023-03-18 12:39:59 +0800

    Update CustomDefault.sh

--- CustomDefault.sh
+++ CustomDefault.sh
@@ -6,3 +6,9 @@ uci commit fstab
 
 # 更换腾讯源
 sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/openwrt#g' /etc/opkg/distfeeds.conf
+
+#开启wifi
+uci set wireless.@wifi-device[0].disabled=0
+uci set wireless.@wifi-device[1].disabled=0
+uci commit wireless
+wifi
