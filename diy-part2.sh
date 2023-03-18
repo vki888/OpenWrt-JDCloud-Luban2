b19ef5e4c2c3e833f3e63ec55e06ccf5093eb09b
vki888 <31002355+vki888@users.noreply.github.com>
2023-03-18 13:11:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,7 +38,7 @@ sed -i 's/yuncore,ax820/jdcloud,luban/g' package/boot/uboot-envtools/files/ramip
 grep all5002 -n5 package/boot/uboot-envtools/files/ramips
 
 echo '-----------------载入 mt7621_jdcloud_luban.dts'
-curl --retry 3 -s --globoff "https://gist.githubusercontent.com/vki888/9311d2b0854849f2f8eb3d89e5fb099b/raw/bd93c664ba1f958c667a010ce5cf5ac0d980cf1b/%255Bopenwrt%255Dmt7621_jdcloud_luban_2.dts" -o target/linux/ramips/dts/mt7621_jdcloud_luban.dts
+curl --retry 3 -s --globoff "https://gist.githubusercontent.com/vki888/9311d2b0854849f2f8eb3d89e5fb099b/raw/fddb015dba232a89511c287da144cc65665dee57/%255Bopenwrt%255Dmt7621_jdcloud_luban_2.dts" -o target/linux/ramips/dts/mt7621_jdcloud_luban.dts
 cat target/linux/ramips/dts/mt7621_jdcloud_luban.dts
 
 # fix2 + fix4.2
